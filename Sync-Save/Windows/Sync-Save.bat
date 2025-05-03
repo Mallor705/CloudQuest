fff44be77c132f6dd30dad07c274089adc3c35a4
Mallor705 <messiasjuniormj705@gmail.com>
2025-05-03 10:19:14 -0300

    Refatora o script para executar o PowerShell de forma oculta usando um arquivo VBS temporário

--- Sync-Save/Windows/Sync-Save.bat
+++ Sync-Save/Windows/Sync-Save.bat
@@ -1,3 +1,6 @@
 @echo off
-powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "F:\messi\Games\Steam\steamapps\common\ELDEN RING\Game\Sync-Save.ps1"
+set "psScript=F:\messi\Games\Steam\steamapps\common\ELDEN RING\Game\Sync-Save.ps1"
+echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\RunHidden.vbs"
+echo WshShell.Run "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File ""%psScript%""", 0, False >> "%temp%\RunHidden.vbs"
+wscript "%temp%\RunHidden.vbs"
 exit
