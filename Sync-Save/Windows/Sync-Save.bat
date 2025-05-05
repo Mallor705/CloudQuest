080a35d1c2955d9e7a744cd97faf569f7d733307
Mallor705 <messiasjuniormj705@gmail.com>
2025-05-05 12:28:28 -0300

    feat: Adiciona novo script MergedConfig.ps1 para configuração de sincronização de saves

--- Sync-Save/Windows/Sync-Save.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "psScript=D:\messi\Documents\GitHub\Sync-Save\Sync-Save\Windows\Sync-Save.ps1"
-echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\RunHidden.vbs"
-echo WshShell.Run "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File ""%psScript%""", 0, False >> "%temp%\RunHidden.vbs"
-wscript "%temp%\RunHidden.vbs"
-exit
